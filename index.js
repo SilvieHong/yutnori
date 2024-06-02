a0136698948da63363009d9f8971f57b22561ae8
SilvieHong <heh9007@gmail.com>
2024-06-02 22:05:40 +0900

    RETRY, HOWTO기능 추가, CSS 수정

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function randomGenerator(seq) {
     .getElementsByClassName(seq)[0]
     .setAttribute("src", randomImageSource);
 }
-randomGenerator("first");
-randomGenerator("second");
-randomGenerator("third");
-randomGenerator("fourth");
+
+function throwYut() {
+  randomGenerator("first");
+  randomGenerator("second");
+  randomGenerator("third");
+  randomGenerator("fourth");
+}
+throwYut();
 
 // 점수계산
 const map = new Map();
@@ -41,7 +45,31 @@ function showResult() {
     step = 5;
   }
   document.querySelector(".score").innerHTML = map.get(score);
-
   document.querySelector(".move").innerHTML = "Move " + step + " step forward!";
+  score = 0;
 }
 showResult();
+score = 0; // 점수초기화
+
+function retryButton() {
+  throwYut();
+  showResult();
+}
+
+// 모달 관련
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+overlay.addEventListener("click", closeModal);
